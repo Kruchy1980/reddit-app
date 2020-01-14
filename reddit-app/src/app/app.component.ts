6c44e0bb541552e765bab7341c3437a009099885
Kruchy1980 <jakub.kruszynski@vp.pl>
2020-01-14 17:53:37 +0100

    Adding articles form and xubmit button added - information in displayed in console

--- reddit-app/src/app/app.component.ts
+++ reddit-app/src/app/app.component.ts
@@ -1,8 +1,14 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core'
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  addArticle(title: HTMLInputElement, link:HTMLInputElement): boolean {
+  // log just to check if button is working
+  console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+  return false;
+  }
+}
